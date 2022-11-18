c4e49dc66fc72b8e6b496ef83376e86810ecc15d
Fer <afriguez@gmail.com>
2022-11-17 22:40:27 -0500

    feat: update index.tsx (home) & add bg image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,33 @@
+import { Button } from "../components/Button";
+import logo from "public/logo.svg";
+import bgImg from "public/image3.png";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div
+      className="m-auto flex h-screen w-screen flex-col bg-blend-soft-light items-center justify-center gap-5 bg-[#c4c4c4]"
+	  style={{ backgroundImage: `url(${bgImg.src})` }}
+    >
+      <img src={logo.src} />
+      <div className="flex w-[200px] flex-col gap-4 md:w-[400px]">
+        <Button
+          content="EAFIT"
+          primary="#0F8B8D"
+          secondary="#0E6566"
+          extraStyle={{ padding: "20px 0 20px 0" }}
+        />
+        <Button
+          content="Iniciar Sesión"
+          primary="#750E6E"
+          secondary="#43083F"
+          extraStyle={{ padding: "20px 0 20px 0" }}
+        />
+      </div>
+
+      <div className="flex flex-row gap-5">
+		<p className="cursor-pointer hover:drop-shadow-lg hover:font-bold">¿Cómo funciona?</p>
+        <p className="cursor-pointer hover:drop-shadow-lg hover:font-bold">Inicio</p>
+      </div>
+    </div>
+  );
 }
