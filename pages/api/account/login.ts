728da398c1bb50568d1359b42a4c491352966236
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-18 00:26:40 -0300

    feat: Implement auth functionality

--- pages/api/account/login.ts
+++ pages/api/account/login.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { signInWithEmailAndPassword } from "firebase/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { RequestMethods } from "../../../enum/RequestMethods";
+import firebaseAdmin from "../../../libs/firebaseAdmin";
+import firebaseApp from "../../../libs/firebaseApp";
 
-type Data = {
-  name: string;
+type LoginResponse = {
+  token: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<LoginResponse>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const method = req.method;
+
+  if (method !== RequestMethods.POST) return res.status(405);
+
+  const { email, password } = req.body;
+
+  const firebaseCredentials = await signInWithEmailAndPassword(
+    firebaseApp.auth,
+    email,
+    password
+  );
+
+  const token = await firebaseAdmin.auth.createCustomToken(
+    firebaseCredentials.user.uid
+  );
+
+  return res.status(200).json({ token });
 }
--- pages/api/account/register.ts
+++ pages/api/account/register.ts
@@ -1,13 +1,62 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { RequestMethods } from "../../../enum/RequestMethods";
+import { UserType } from "../../../enum/UserType";
+import firebaseAdmin from "../../../libs/firebaseAdmin";
+import firebaseApp from "../../../libs/firebaseApp";
+import prisma from "../../../libs/prisma";
 
-type Data = {
-  name: string;
+type RegisterResponse = {
+  token: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<RegisterResponse>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const method = req.method;
+
+  if (method !== RequestMethods.POST) return res.status(405);
+
+  const { userType, ...body } = req.body;
+
+  if (body.password !== body.confirmPassword) return res.status(400);
+
+  const firebaseCredentials = await createUserWithEmailAndPassword(
+    firebaseApp.auth,
+    body.email,
+    body.password
+  );
+
+  const token = await firebaseAdmin.auth.createCustomToken(
+    firebaseCredentials.user.uid
+  );
+
+  switch (userType) {
+    case UserType.PROFESSOR: {
+      await prisma.professor.create({
+        data: {
+          name: body.name,
+          surname: body.surname,
+          id: firebaseCredentials.user.uid,
+        },
+      });
+      break;
+    }
+
+    case UserType.STUDENT: {
+      await prisma.student.create({
+        data: {
+          id: firebaseCredentials.user.uid,
+          username: body.username,
+        },
+      });
+      break;
+    }
+    default:
+      return res.status(400);
+  }
+
+  return res.status(200).send({ token });
 }
